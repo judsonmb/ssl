59cd96270c6b86ef11b4d83b9584c64ea9b22c46
Judson Bandeira <judsonmelobandeira@gmail.com>
2019-11-08 10:32:44 -0300

    tentando executar de uma por uma migração

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->enum('type', ['administrador', 'parceiro', 'solicitante']);
+            $table->bigInteger('institution_id')->unsigned();
+            $table->foreign('institution_id')->references('id')->on('Institutions')->ondelete('cascade');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password')->default('$2y$10$IuXbbX7F6ma715YhJLnzFeRkeyHVk5b32shvh8EZynrVKmN68joOe');
+            $table->rememberToken();
+            $table->timestamps();
+			$table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/migrations/2019_10_24_230646_create_requests_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('Users');
+            $table->bigInteger('project_id')->unsigned();
+            $table->foreign('project_id')->references('id')->on('Projects')->ondelete('cascade');
+            $table->string('title');
+            $table->text('description');
+            $table->bigInteger('technician_id')->unsigned()->nullable();
+            $table->foreign('technician_id')->references('id')->on('Users');
+            $table->enum('type', ['bug', 'nova funcionalidade', 'melhoria', 'pedido'])->nullable();
+            $table->enum('priority', ['crítica', 'maior', 'menor', 'leve'])->nullable();
+            $table->date('deadline')->nullable();
+            $table->enum('status', ['a fazer', 'fazendo', 'bloqueada', 'feita'])->default('a fazer');
+            $table->enum('delivered', ['dentro do prazo', 'atrasado'])->nullable();
+            $table->integer('function_points')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+}
