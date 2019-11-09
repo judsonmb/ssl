157ffa94ae7dfce20e9d6a63c724bb8a363e6769
Judson Bandeira <judsonmelobandeira@gmail.com>
2019-11-09 16:13:46 -0300

    corrigindo valor da variavel

--- resources/views/emails/requestmessage.blade.php
+++ resources/views/emails/requestmessage.blade.php
@@ -2,7 +2,8 @@
 
 A solicitação sobre <i><strong>{{$requestTitle}}</strong></i> recebeu uma mensagem!<br><br>
 
-<h4>{ $messageMail }</h4><br><br>
+<h4>{{ $messageMail }}</h4><br><br>
+
 
 Clique <a href="ssl.linkn.com.br/requests/{{ $requestId }}">aqui</a> para ver mais detalhes.
 </div>
